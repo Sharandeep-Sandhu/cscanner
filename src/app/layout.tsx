935276197042889071021fe2f397eeb808453336
Sharandeep Singh <onlypythonuse@gmail.com>
2024-06-25 15:58:31 +0530

    changed 10 files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Component from "@/components/component/component";
-import Search from"@/components/component/search"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Component />
-        {/* <Search /> */}
+
       </body>
     </html>
   );
