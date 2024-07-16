bb4ddd7077754ca2eb996b1b75800b920129a529
Sharandeep Singh <onlypythonuse@gmail.com>
2024-07-16 13:07:29 +0530

    modified 3 files

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,29 +1,24 @@
 import React from "react";
 import Link from "next/link";
-import { MenuIcon } from "lucide-react";
-import { Button } from "./button";
 import Image from 'next/image';
 
 function Navbar() {
     return (
         <header className="bg-black text-white py-4 px-6 md:px-8 lg:px-10">
             <div className="mx-auto flex items-center justify-between space-x-2">
-                <Link href="/" className="font-bold text-[35px] flex items-center">
+                <Link href="/" className="font-bold text-3xl flex items-center">
                     <Image
-                        src="/logo/logo1.png" 
+                        src="/logo/logo1.png"
                         alt="Logo"
                         width={200}
                         height={200}
-                        className="mr-2 mt--9"
+                        className="mr-2"
                     />
-                    {/* <img src="\logo\logo1.png" alt="Logo" width={200} height={200} className="mr-2 mt--9" /> */}
-                    {/* Course Scanner */}
+
                 </Link>
             </div>
-
         </header>
-
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
